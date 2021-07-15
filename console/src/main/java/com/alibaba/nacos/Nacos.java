9117d7cd73f3e0fdb0e2d980aa9b7a4ac377e107
secbro <xinyoulingxi2008@126.com>
2021-07-15 21:07:30 +0800

    设置为单机启动

--- console/src/main/java/com/alibaba/nacos/Nacos.java
+++ console/src/main/java/com/alibaba/nacos/Nacos.java
@@ -16,6 +16,7 @@
 
 package com.alibaba.nacos;
 
+import com.alibaba.nacos.sys.env.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -32,6 +33,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Nacos {
     
     public static void main(String[] args) {
+        // 通过环境变量的形式设置单机启动
+        System.setProperty(Constants.STANDALONE_MODE_PROPERTY_NAME, "true");
+        // 通过环境变量的形式设置关闭权限校验
+        System.setProperty("nacos.core.auth.enabled", "false");
+
         SpringApplication.run(Nacos.class, args);
     }
 }
